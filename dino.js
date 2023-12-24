fc151dcd055b96af12b2f81c869c04be50d8373d
Allancool9 <TheOfficialAllanRamirez@gmail.com>
2023-12-23 22:02:20 -0500

    so far so good, got cactus and dino

--- dino.js
+++ dino.js
@@ -15,16 +15,98 @@ let dino = {
     x : dinoX,
     y : dinoY,
     width : dinoWidth,
-    height : dinoHeight
+    height : dinoHeight,
 }
-// when I was testing my code for the axis of the dino
-// window.onload = function() {
-//     board = document.getElementById("board");
-//     board.height = boardHeight;
-//     board.width = boardWidth;
 
-//     context = board.getContext("2d")
+//our cactus 
+let cactusArray = [];
 
-// context.fillStyle ="green";
-// context.fillRect(dino.x, dino.y, dino.width, dino.height);
-// }
+let cactus1Width = 34;
+let cactus2Width = 69;
+let cactus3Width = 102;
+
+let cactusHeight = 70;
+let cactusX = 700;
+let cactusY = boardHeight - cactusHeight;
+
+
+let cactus1Img;
+let cactus2Img;
+let cactus3Img;
+
+
+
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+
+    // when I was testing my code for the axis of the dino
+    // context.fillStyle ="green";
+    // context.fillRect(dino.x, dino.y, dino.width, dino.height);
+    
+    context = board.getContext("2d")
+
+    dinoImg = new Image();
+    dinoImg.src = "./img/dino.png";
+    dinoImg.onload = function() {
+        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+    }
+    cactus1Img = new Image();
+    cactus1Img.src = "./img/cactus1.png"
+    
+    
+    cactus2Img = new Image();
+    cactus2Img.src = "./img/cactus2.png"
+
+
+    cactus3Img = new Image();
+    cactus3Img.src = "./img/cactus3.png"
+
+    requestAnimationFrame(update)
+    setInterval(placeCactus, 1000) // saying for every 100 milliseconds = 1 second
+}
+
+// this is used for drawing the frames for our game 
+function update(){
+    requestAnimationFrame(update);
+    //dino
+    context.drawImage(dinoImg, dino.x,dino.y,dino.width,dino.height);
+
+    //cactus 
+    for (let i = 0; i < cactusArray.length; i++){
+        let cactus = cactusArray[i];
+        context.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height);
+    }
+}
+//generate our cactuses
+ function placeCactus() {
+ // place cactus 
+ let cactus = {
+    img : null,
+    x : cactusX,
+    y : cactusY,
+    width : null,
+    height : cactusHeight
+
+}
+    let placeCactusChance = Math.random();
+
+    if (placeCactusChance > .90) /*10 percent */ {
+       cactus.img = cactus3Img;
+       cactus.width = cactus3Width;
+       cactusArray.push(cactus);
+    } 
+    else if(placeCactusChance > .70) /* 30percent */ {
+        cactus.img = cactus2Img;
+        cactus.width = cactus2Width;
+        cactusArray.push(cactus);
+    }
+     else if (placeCactusChance > .50)  /* 50 percent */ {
+        cactus.img = cactus1Img;
+        cactus.width = cactus1Width;
+        cactusArray.push(cactus);
+     }
+ }
+ 
